b35745426e2ccfde21ddcb5ecb4d480de0aab0ec
zesettler <gromovgrin@gmail.com>
2023-06-23 22:40:49 +0300

    commit

---
+++ components/Hero.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+    const handleScroll = () => {
+
+    }
+
+    return (
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Find, book or rent a car -- quickly and easily!
+                </h1>
+                <p className="hero__subtitle">
+                    Streamline your car rental experience with our effortle booking process.
+                </p>
+                <CustomButton 
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue taxt-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
