b17ab84559fa1828372823856286fc055d152213
zesettler <gromovgrin@gmail.com>
2023-06-25 23:22:48 +0300

    car cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { CarCard } from "@/components";
 import CustomFilter from "@/components/CustomFilter";
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -20,6 +26,20 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car}/>
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">OOps, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
---
+++ utils/index.ts
@@ -0,0 +1,14 @@
+export async function fetchCars() {
+    const headers = {
+        'X-RapidAPI-Key': 'cceced3648msh6253de68d049534p1a8c6djsnfe60baf62a98',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+        headers: headers,
+    });
+
+    const result = await response.json();
+
+    return result;
+}
