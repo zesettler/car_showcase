63113b5e534ccb54651307853f88709fba3ffd24
zesettler <gromovgrin@gmail.com>
2023-06-28 20:53:26 +0300

    car cards fix

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,9 +3,10 @@
 import { useState } from "react";
 import Image from "next/image";
 
-import { calculateCarRent } from "@/utils";
-import CustomButton from "./CustomButton";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -33,7 +34,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
 
       <div className='relative w-full h-40 my-3 object-contain'>
-        <Image src={(car)} alt='car model' fill priority className='object-contain' />
+        <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
       </div>
 
       <div className='relative flex w-full mt-2'>
@@ -65,6 +66,7 @@ const CarCard = ({ car }: CarCardProps) => {
         </div>
       </div>
 
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
     </div>
   );
 };
